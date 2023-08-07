1b85b552a8e33689bad760a0737e11fe48850e77
dard23 <141675448+dard23@users.noreply.github.com>
2023-08-07 16:47:18 -0300

    Initial commit

---
+++ src/main/java/dario/probandogithub/ProbandoGitHub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package dario.probandogithub;
+
+/**
+ *
+ * @author dard
+ */
+public class ProbandoGitHub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/dario/probandogithub/cliente.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dario.probandogithub;
+
+/**
+ *
+ * @author dard
+ */
+public class cliente {
+    
+}
